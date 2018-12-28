8bc2801a5c2536a076260f9f055b986b89cda9b8
Derek Anderson <derek@armyofevilrobots.com>
2018-12-28 13:59:29 -0800

    Refactor/Cleanup
     - Break each part of the pong lib into standalone files
     - Some misc. import cleanup for less complaints.

--- src/pong/mod.rs
+++ src/pong/mod.rs
@@ -1,23 +1,21 @@
 extern crate amethyst;
 
+mod ball;
+mod constants;
+mod paddles;
+mod scoreboard;
 mod sprites;
-use crate::pong::sprites::load_sprite_sheet;
-use amethyst::core::transform::Transform;
-use amethyst::prelude::*;
-use amethyst::{
-    assets::Loader,
-    ecs::prelude::{Component, DenseVecStorage, Entity},
-    renderer::{Camera, Flipped, Projection, SpriteRender, SpriteSheetHandle},
-    ui::{Anchor, TtfFormat, UiText, UiTransform},
+pub use crate::pong::{
+    ball::{initialise_ball, Ball},
+    constants::*,
+    paddles::*,
+    scoreboard::{initialise_scoreboard, ScoreBoard, ScoreText},
+    sprites::load_sprite_sheet,
 };
 
-pub const BALL_VELOCITY_X: f32 = 40.0;
-pub const BALL_VELOCITY_Y: f32 = 30.0;
-pub const BALL_RADIUS: f32 = 2.0;
-pub const ARENA_HEIGHT: f32 = 100.0;
-pub const ARENA_WIDTH: f32 = 100.0;
-pub const PADDLE_HEIGHT: f32 = 16.0;
-pub const PADDLE_WIDTH: f32 = 4.0;
+use amethyst::core::transform::Transform;
+use amethyst::prelude::*;
+use amethyst::renderer::{Camera, Projection};
 
 pub struct Pong;
 
@@ -50,155 +48,4 @@ fn initialise_camera(world: &mut World) {
         )))
         .with(transform)
         .build();
-}
-
-#[derive(PartialEq, Eq)]
-pub enum Side {
-    Left,
-    Right,
-}
-
-pub struct Paddle {
-    pub side: Side,
-    pub width: f32,
-    pub height: f32,
-}
-
-impl Paddle {
-    fn new(side: Side) -> Paddle {
-        Paddle {
-            side,
-            width: PADDLE_WIDTH,
-            height: PADDLE_HEIGHT,
-        }
-    }
-}
-
-impl Component for Paddle {
-    type Storage = DenseVecStorage<Self>;
-}
-
-fn initialise_paddles(world: &mut World, sprite_sheet: SpriteSheetHandle) {
-    let mut left_transform = Transform::default();
-    let mut right_transform = Transform::default();
-
-    // Get the sprite stuff prepped for below.
-    let sprite_render = SpriteRender {
-        sprite_sheet: sprite_sheet.clone(),
-        sprite_number: 0, // First sprite in dat shit!
-    };
-
-    // Correctly position the paddles...
-    let y = ARENA_HEIGHT / 2.0;
-    left_transform.set_xyz(PADDLE_WIDTH * 0.5, y, 0.0);
-    right_transform.set_xyz(ARENA_WIDTH - PADDLE_WIDTH * 0.5, y, 0.0);
-
-    //Create the left plank entity
-    world
-        .create_entity()
-        .with(sprite_render.clone())
-        .with(Paddle::new(Side::Left))
-        .with(left_transform)
-        .build();
-
-    //Create the right plank entity
-    world
-        .create_entity()
-        .with(sprite_render.clone())
-        .with(Flipped::Horizontal)
-        .with(Paddle::new(Side::Right))
-        .with(right_transform)
-        .build();
-}
-
-pub struct Ball {
-    pub velocity: [f32; 2],
-    pub radius: f32,
-}
-
-impl Component for Ball {
-    type Storage = DenseVecStorage<Self>;
-}
-
-fn initialise_ball(world: &mut World, sprite_sheet_handle: SpriteSheetHandle) {
-    let mut local_transform = Transform::default();
-    local_transform.set_xyz(ARENA_WIDTH / 2.0, ARENA_HEIGHT / 2.0, 0.0);
-
-    //Assign the sprite...
-    let sprite_render = SpriteRender {
-        sprite_sheet: sprite_sheet_handle,
-        sprite_number: 1,
-    };
-
-    world
-        .create_entity()
-        .with(sprite_render)
-        .with(Ball {
-            radius: BALL_RADIUS,
-            velocity: [BALL_VELOCITY_X, BALL_VELOCITY_Y],
-        })
-        .with(local_transform)
-        .build();
-}
-
-#[derive(Default)]
-pub struct ScoreBoard {
-    pub score_left: i32,
-    pub score_right: i32,
-}
-
-pub struct ScoreText {
-    pub p1_score: Entity,
-    pub p2_score: Entity,
-}
-
-fn initialise_scoreboard(world: &mut World) {
-    let font = world.read_resource::<Loader>().load(
-        "resources/fonts/square.ttf",
-        TtfFormat,
-        Default::default(),
-        (),
-        &world.read_resource(),
-    );
-    let p1_transform = UiTransform::new(
-        "P2".to_string(),
-        Anchor::TopMiddle,
-        -50.,
-        -50.,
-        1.,
-        200.,
-        50.,
-        0,
-    );
-    let p2_transform = UiTransform::new(
-        "P2".to_string(),
-        Anchor::TopMiddle,
-        50.,
-        -50.,
-        1.,
-        200.,
-        50.,
-        0,
-    );
-    let p1_score = world
-        .create_entity()
-        .with(p1_transform)
-        .with(UiText::new(
-            font.clone(),
-            "0".to_string(),
-            [1., 1., 1., 1.],
-            50.,
-        ))
-        .build();
-    let p2_score = world
-        .create_entity()
-        .with(p2_transform)
-        .with(UiText::new(
-            font.clone(),
-            "0".to_string(),
-            [1., 1., 1., 1.],
-            50.,
-        ))
-        .build();
-    world.add_resource(ScoreText { p1_score, p2_score });
 }
